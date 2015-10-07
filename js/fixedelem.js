67bd30b15a1f567c0de1dfb646e1390277d36283
brandonfujii <brandonfujii2018@u.northwestern.edu>
2015-10-07 16:09:34 -0500

    separated js files

---
+++ js/fixedelem.js
@@ -0,0 +1,38 @@
+$(document).scroll(function(){
+	var $navBar = $(".nav-container");
+	var scrollPos = $(this).scrollTop();
+
+	if (scrollPos > 0) {
+		$navBar.addClass("active");
+	} else {
+		$navBar.removeClass("active");
+	}
+});
+
+var $button = $(".button");
+
+$button.on("click", function() {
+	var $sect = $("#" + $(this).data("nav"));
+	var $navHeight = $(".nav-container nav").height();
+
+	if ($sect) {
+		$sect.velocity("scroll", {duration: 750, offset: -$navHeight});
+	}
+});
+
+var $socialIcon = $(".connect ul li a"); 
+
+$socialIcon.on("mouseover", function() {
+	$(this).velocity("stop");
+	$(this).velocity({
+		top: -20
+	}, 200);
+})
+
+$socialIcon.on("mouseleave", function() {
+	$(this).velocity("stop");
+	$(this).velocity({
+		top: 0
+	}, 200);
+})
+
--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
-$(document).scroll(function(){
-	var $navBar = $(".nav-container");
-	var scrollPos = $(this).scrollTop();
-
-	if (scrollPos > 0) {
-		$navBar.addClass("active");
-	} else {
-		$navBar.removeClass("active");
-	}
-});
 
 $('[typer]').typer();
+
+var $button = $(".button");
+
